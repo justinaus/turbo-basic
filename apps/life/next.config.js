95bef3281dff3c3d0f44143b0f5128a531760e9b
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2023-09-08 16:53:47 +0900

    Add(life) emotion css props

--- apps/life/next.config.js
+++ apps/life/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ["ui"],
-}
+  reactStrictMode: false,
+  transpilePackages: ['ui'],
+  compiler: {
+    emotion: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
