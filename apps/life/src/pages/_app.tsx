94301e58c91f6237accc8742cb993aab8ca9c96d
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2023-09-11 19:31:37 +0900

    Add(life) recoil

--- apps/life/src/pages/_app.tsx
+++ apps/life/src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
 import { suitRegular } from '@/fonts/suitRegular';
 import AppLayout from '@/components/layout/AppLayout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AppLayout>
-      <main className={suitRegular.className}>
-        <Component {...pageProps} />
-      </main>
-    </AppLayout>
+    <RecoilRoot>
+      <AppLayout>
+        <main className={suitRegular.className}>
+          <Component {...pageProps} />
+        </main>
+      </AppLayout>
+    </RecoilRoot>
   );
 }
