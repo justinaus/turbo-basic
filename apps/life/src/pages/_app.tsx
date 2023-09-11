3e385c375cffb95c4339f0b14915338dedd1257f
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2023-09-11 19:48:18 +0900

    Add(life) users page

--- apps/life/src/pages/_app.tsx
+++ apps/life/src/pages/_app.tsx
@@ -1,17 +1,23 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
+import Head from 'next/head';
 import { suitRegular } from '@/fonts/suitRegular';
 import AppLayout from '@/components/layout/AppLayout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <AppLayout>
-        <main className={suitRegular.className}>
-          <Component {...pageProps} />
-        </main>
-      </AppLayout>
-    </RecoilRoot>
+    <>
+      <Head>
+        <meta content="initial-scale=1, width=device-width" name="viewport" />
+      </Head>
+      <RecoilRoot>
+        <AppLayout>
+          <main className={suitRegular.className}>
+            <Component {...pageProps} />
+          </main>
+        </AppLayout>
+      </RecoilRoot>
+    </>
   );
 }
---
+++ apps/life/src/pages/users/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Users() {
+  return (
+    <>
+      <Head>
+        <title>Users</title>
+        <meta content="User list" name="description" />
+      </Head>
+      <ul>Users</ul>
+    </>
+  );
+}
