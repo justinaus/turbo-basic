d536ba4225b019f67e2d88316c18fed1366ab5b9
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2023-09-11 20:27:11 +0900

    Fix(life) lint - css props

--- apps/life/.eslintrc.js
+++ apps/life/.eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
   extends: ['custom/next', 'plugin:@tanstack/eslint-plugin-query/recommended'],
+  rules: {
+    'react/no-unknown-property': ['error', { ignore: ['css'] }],
+  },
   settings: {
     next: {
       rootDir: 'apps/life/',
