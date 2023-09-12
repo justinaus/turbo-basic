771af9ae009bc120ebb35c9ddfbb5c70918f9b9c
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2023-09-12 14:18:44 +0900

    Update emotion tips

--- apps/life/src/components/users/List.tsx
+++ apps/life/src/components/users/List.tsx
@@ -36,6 +36,7 @@ export default function List() {
   return (
     <ul
       css={{
+        // emotion을 사용하는 구성 요소 인스턴스의 수를 줄이세요. 구성 요소 인스턴스 각각에 대해 소량의 작업을 해야 한다. 더 성능이 뛰어난 접근 방식은 CSS 선택기를 사용하여 동일한 CSS 조각으로 10,000개의 요소 각각을 대상으로 하는 단일 부모 요소에 css prop을 사용하는 것입니다.
         li: {
           color: 'red',
         },
---
+++ apps/life/src/pages/test/index.tsx
@@ -0,0 +1,68 @@
+import { css } from '@emotion/react';
+import Head from 'next/head';
+import { colors } from 'ui';
+
+// https://emotion.sh/docs/best-practices#consider-defining-styles-outside-your-components
+const cardCss = {
+  self: css({
+    backgroundColor: 'white',
+    border: '1px solid #eee',
+    borderRadius: '0.5rem',
+    padding: '1rem',
+  }),
+
+  title: css({
+    fontSize: '1.25rem',
+  }),
+};
+
+export const errorCss = css({
+  color: 'red',
+  fontWeight: 'bold',
+});
+
+// Use arrays to compose styles
+export const largeErrorCss = css([errorCss, { fontSize: '1.5rem' }]);
+
+// https://emotion.sh/docs/best-practices#use-the-style-prop-for-dynamic-styles
+export default function Test() {
+  return (
+    <>
+      <Head>
+        <title>Test</title>
+        <meta content="Users" name="description" />
+      </Head>
+      <div>
+        <div
+          css={{
+            color: 'var(--background-style)',
+          }}
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any -- test.
+          style={{ ['--background-style' as any]: 'blue' }}
+        >
+          test1
+        </div>
+        <div
+          css={{
+            color: 'var(--background-style)',
+          }}
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any -- test.
+          style={{ ['--background-style' as any]: 'green' }}
+        >
+          test2
+        </div>
+        <div css={cardCss.self}>
+          <h5 css={cardCss.title}>4444</h5>
+          333
+        </div>
+        <div
+          css={{
+            color: colors.success,
+          }}
+        >
+          123
+        </div>
+      </div>
+    </>
+  );
+}
